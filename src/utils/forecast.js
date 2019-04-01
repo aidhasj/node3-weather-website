4b77a6123117d0a860c0ea7310039b3010ee7c53
unknown <you@example.com>
2019-04-01 14:59:03 +0700

    Forecast has been updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,7 +20,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + 'The Highest temperature is ' + body.daily.data[0].temperatureHigh + '. The lowest temperature is ' + body.daily.data[0].temperatureLow + '. It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.' + 'Today the Highest temperature is ' + body.daily.data[0].temperatureHigh + ' and the lowest temperature is ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
