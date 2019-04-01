1831273a46d6111d7c2f19b722a480c592a88804
unknown <you@example.com>
2019-04-01 10:49:43 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const forecast = require('./utils/forecast')
 //3. Test your work by visiting all three pages
 
 const app = express()
+const port = proces.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -177,6 +178,6 @@ app.get('*', (req, res) => {
 //  -- Help article not found.
 //4. Test your work. Visit /what and /help/units
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port) 
 })
