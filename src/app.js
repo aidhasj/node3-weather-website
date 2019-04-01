743bd8cd1bf1fb12decafffc42ed466c1b941c7b
unknown <you@example.com>
2019-04-01 11:26:53 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const forecast = require('./utils/forecast')
 //3. Test your work by visiting all three pages
 
 const app = express()
-const port = proces.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
